4ef48d70652fdcb217b499861b38da290b456e2a
muckystack <muckystack@gmail.com>
2021-01-04 16:12:09 -0600

    FEAT: Primer ruta

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+// Rutas
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+    msg: 'Todo bien'
+  })
+});
+
 app.listen(3000, () => {
   console.log("Servidor Corriendo en puerto 3000");
 });
