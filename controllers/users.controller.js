b66af2777db5cc1be1663a1becd79a967c6c96e7
muckystack <muckystack@gmail.com>
2021-01-04 22:52:58 -0600

    FEAT: Validación de correo

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,24 +1,37 @@
+const { response } = require("express");
 const User = require("../models/users.model");
 
 const getUsers = async (req, res) => {
-  const users = await User.find({}, 'name mail role google');
+  const users = await User.find({}, "name mail role google");
+
   res.json({
     ok: true,
     users,
   });
 };
 
-const createUser = async (req, res) => {
+const createUser = async (req, res = response) => {
   const { name, email, password } = req.body;
 
-  const user = new User(req.body);
+  try {
+    const existEmail = await User.findOne({ email });
 
-  await user.save();
+    if (existEmail)
+      return res
+        .status(400)
+        .json({ ok: false, msg: "El correo ya esta registrado" });
 
-  res.json({
-    ok: true,
-    user,
-  });
+    const user = new User(req.body);
+    await user.save();
+
+    res.json({
+      ok: true,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ ok: false, msg: "Error inesperado, revisa logs" });
+  }
 };
 
 module.exports = {
